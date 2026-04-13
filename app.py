806a743844e41b048b972a9d579ef7b2f5fc07a8
Ubuntu <ubuntu@ip-172-31-71-171.ec2.internal>
2026-04-13 13:45:25 +0000

    Fixing

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "this web_site actually fixed and ready for work🔥🔥💻"
+    return "this web_site is actually fixed and ready for work🔥🔥💻"
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port="5000", debug=True)

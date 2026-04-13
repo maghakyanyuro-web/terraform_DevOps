040771f4ead7e84ff634ea51b10a9ebded6d00c6
Ubuntu <ubuntu@ip-172-31-71-171.ec2.internal>
2026-04-13 14:07:50 +0000

    kapik

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "this web_site is actually fixed and ready for work🔥🔥💻"
+    return "Barev im exbayr Ruben"
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port="5000", debug=True)
